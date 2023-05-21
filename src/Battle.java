cb6dcdff3ff8b5d23ef1f054beca497562cbb362
Artyom Pionov <pionovartem11@gmail.com>
2023-05-21 22:29:09 +0100

    First Commit: Working on a game.

--- src/Battle.java
+++ src/Battle.java
@@ -1,10 +1,61 @@
-/* При этом, если наша ловкость, умноженная на 3, больше, чем случайное значение (от 0 до 100),
-   то мы атакуем в размере нашей силы, если нет, то возвращаем 0, что приравнивается к промаху.
-
-   Если игрок убит —игра заканчивается,
+/*
+   Если игрок убит — игра заканчивается,
    если убит монстр — игрок получает золото и опыт.
    Бой нужно реализовать в отдельном потоке.
 */
 
+@SuppressWarnings("ReassignedVariable")
 public class Battle {
+    void fight(Character player, Character monster, Game.FightCallback fightCallback) {
+        Runnable runnable = () -> {
+            int turn = 1;
+            boolean isFightEnded = false;
+            while (!isFightEnded) {
+                System.out.println("----Turn: " + turn + "----");
+                if (turn++ % 2 != 0) {
+                    isFightEnded = makeHit(monster, player, fightCallback);
+                } else {
+                    isFightEnded = makeHit(player, monster, fightCallback);
+                }
+                try {
+                    //Чтобы бой не проходил за секунду, сделаем имитацию работы, как если бы
+                    //у нас была анимация
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        // Starting fight thread.
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+    boolean makeHit(Character attacker, Character defender, Game.FightCallback fightCallback) {
+        int hit = attacker.attack();
+        int defenderHealth = defender.getHp();
+
+        if (hit != 0) {
+            System.out.printf("%s did %d damage!%n", attacker.getName(), hit);
+            System.out.printf("%s has %d health left...%n", defender.getName(), defenderHealth);
+        } else {
+            System.out.printf("%s missed!%n", attacker.getName());
+        }
+        if (defenderHealth <= 0 && defender instanceof Player) {
+            System.out.println("Lost to the Zone...");
+
+            fightCallback.fightLost();
+            return true;
+        } else if (defenderHealth <= 0) {
+            System.out.printf("Enemy defeated! You got %d XP and %d Gold%n", defender.getXp(), defender.getGold());
+            attacker.setXp(attacker.getXp() + defender.getXp());
+            attacker.setGold(attacker.getGold() + defender.getGold());
+
+            fightCallback.fightWin();
+            return true;
+        } else {
+            defender.setHp(defenderHealth);
+            return false;
+        }
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,107 @@
 /*
-* При запуске игры пользователю нужно будет первым делом создать персонажа, которого назовет пользователь.
-* После этого у игрока должна появиться надпись Куда вы хотите пойти? и быть доступны следующие варианты:
-* 1. К торговцу
-* 2. В тёмный лес
-* 3. На выход
-*
-* В первом случае можно будет попасть к торговцу (которого вам нужно реализовать самостоятельно,
-* вместе с логикой взаимодействия с ним; если еще не реализовали — пусть возвращается надпись Торговец еще не вышел на работу).
-* Во втором случае можно будет запустить логику ведения боя. Монстр должен появляться рандомный (либо скелет, либо гоблин с вероятностью 50%).
-* По завершении торговли или завершении боя должна быть доступна опция Вернуться в город или Продолжить торговлю/бой.
-* */
+ * При запуске игры пользователю нужно будет первым делом создать персонажа, которого назовет пользователь.
+ * После этого у игрока должна появиться надпись Куда вы хотите пойти? И быть доступны следующие варианты:
+ * 1. К торговцу
+ * 2. В тёмный лес
+ * 3. На выход
+ *
+ * В первом случае можно будет попасть к торговцу (которого вам нужно реализовать самостоятельно,
+ * вместе с логикой взаимодействия с ним; если еще не реализовали — пусть возвращается надпись Торговец еще не вышел на работу).
+ * Во втором случае можно будет запустить логику ведения боя. Монстр должен появляться рандомный (либо скелет, либо гоблин с вероятностью 50%).
+ * По завершении торговли или завершении боя должна быть доступна опция Вернуться в город или Продолжить торговлю/бой.
+ * */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Game {
-    public static void main(String[] args) {
+    private static BufferedReader br;
+    private static Character player = null;
+    private static Battle battle = null;
 
+    public static void main(String[] args) {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        battle = new Battle();
+
+        System.out.println("Enter Player Name: ");
+
+        try {
+            command(br.readLine());
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static void command(String string) throws IOException {
+        if (player == null) {
+            player = new Player(string, 100, 20, 20, 0, 0);
+            System.out.printf("%s will save the world from dragons!%n", player.getName());
+
+            printNavigation();
+        }
+        //Варианты для команд
+        switch (string) {
+            case "1" -> {
+                System.out.println("Trader isn't here yet.");
+                command(br.readLine());
+            }
+            case "2" -> commitFight();
+            case "3" -> System.exit(1);
+            case "Yes" -> command("2");
+            case "No" -> {
+                printNavigation();
+                command(br.readLine());
+            }
+        }
+
+        command(br.readLine());
+    }
+
+    private static void printNavigation() {
+        System.out.println("---MENU---");
+        System.out.println("1. To Trader");
+        System.out.println("2. To Dark Forest");
+        System.out.println("3. Exit");
+    }
+
+    private static void commitFight() {
+        battle.fight(player, createMonster(), new Game.FightCallback() {
+            @Override
+            public void fightWin() {
+                System.out.printf("%s won! Now you have %d XP and %d Gold, " + "and %d HP left.%n", player.getName(), player.getXp(), player.getGold(), player.getHp());
+                System.out.println("Wanna continue or go back to town? (Yes/No)");
+                try {
+                    command(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void fightLost() {
+                System.out.println("Wish to continue? (Yes/No)");
+                try {
+                    command(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private static Character createMonster() {
+        int random = (int) (Math.random() * 10);
+
+        if (random % 2 == 0) {
+            return new Goblin("Goblin", 50, 10, 10, 100, 20);
+        } else {
+            return new Skeleton("Skeleton", 25, 20, 20, 100, 10);
+        }
+    }
+
+    interface FightCallback {
+        void fightWin();
+        void fightLost();
     }
 }
